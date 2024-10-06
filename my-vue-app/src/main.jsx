a40a5871fea2169d18ac21dee771d4fb552babd0
shagufa-anjum1 <shagufa.anjum2021@vitbhopal.ac.in>
2024-10-07 03:03:52 +0530

    modified:   my-vue-app/src/main.jsx

--- my-vue-app/src/main.jsx
+++ my-vue-app/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react'; // Import React for createElement
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App!</h1>
+    </div>
+  );
+}
+const anotheruser = "code with sofi"
+const reactElement = React.createElement(
+  'a',
+  { href: "https://google.com", target: "_blank" },
+  "click me to visit",
+  anotheruser
+);
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit google
+  </a>
+);
+
+createRoot(document.getElementById('root')).render(reactElement);
 
 
-createRoot(document.getElementById('root')).render(
-  
-  <App />
-  
-)
