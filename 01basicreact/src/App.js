d2d7ae5a3c1a615bf181d96b00d4fa5db1e3bd56
shagufa-anjum1 <shagufa.anjum2021@vitbhopal.ac.in>
2024-10-06 11:41:55 +0530

    Lecture 01

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+   <div>
+      <h1>Code With Sofi! || SHAGUFA ANJUM </h1>
+      <h2>Today’s React Learning!</h2>
+      <ul>
+      <li>Installed Node.js & npm</li>
+        <li>Created my first React app using npx</li>
+        <li>Explored JSX for writing HTML in JS</li>
+        <li>Installed react-router-dom for routing</li>
+        <li>Learned to build for production with npm run build</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
