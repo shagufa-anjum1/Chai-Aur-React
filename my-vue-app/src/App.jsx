3a9f64bc3e98b41277ad4496de2ad9c3169cbc79
shagufa-anjum1 <shagufa.anjum2021@vitbhopal.ac.in>
2024-10-07 03:02:54 +0530

    modified:   my-vue-app/src/App.jsx

--- my-vue-app/src/App.jsx
+++ my-vue-app/src/App.jsx
@@ -1,13 +1,15 @@
 import Sofi from "./sofi"
 
 function App() {
-  
+
+  const username = "shagufa anjum !"
+  const userMy = "yes bolo na"
 
   return (
     <>
    <Sofi/>
-   <h1>Hello !</h1>
-   <p>hey sofi!!</p>
+   <h1>Hello ! {username}</h1>
+   <p>hey sofi!! {userMy}</p>
    </>
   )
 }
